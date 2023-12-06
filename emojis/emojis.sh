ee838eeca59e6a3d222921f8b47528efad4fee5c
Torsten Römer <torsten.roemer@luniks.net>
2023-12-06 01:36:49 +0100

    Give more time to clear the display

--- emojis/emojis.sh
+++ emojis/emojis.sh
@@ -5,7 +5,7 @@ stty -F /dev/ttyUSB0 speed 38400 cs8 -parenb -cstopb raw
 
 # clear the display all black and write all the emojis...
 echo "c ffff" > /dev/ttyUSB0
-sleep 0.1
+sleep 1
 echo -e "t 0 0 Hello Emojis!" > /dev/ttyUSB0
 sleep 0.1
 echo -e 't 16 0 \ts' > /dev/ttyUSB0
