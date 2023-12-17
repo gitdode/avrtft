054661196da04f4f5ba0e949736cecfcc75d50c6
Torsten Römer <torsten.roemer@luniks.net>
2023-12-17 20:47:46 +0100

    Updated NetBeans configuration

--- nbproject/Makefile-Custom.mk
+++ nbproject/Makefile-Custom.mk
@@ -41,10 +41,10 @@ OBJECTFILES= \
 	${OBJECTDIR}/_ext/48b9b4a1/emojis.o \
 	${OBJECTDIR}/_ext/48b9b4a1/font.o \
 	${OBJECTDIR}/_ext/48b9b4a1/hack.o \
+	${OBJECTDIR}/_ext/48b9b4a1/i2c.o \
 	${OBJECTDIR}/_ext/48b9b4a1/spi.o \
 	${OBJECTDIR}/_ext/48b9b4a1/tft.o \
-	${OBJECTDIR}/_ext/48b9b4a1/touch.o \
-	${OBJECTDIR}/i2c.o
+	${OBJECTDIR}/_ext/48b9b4a1/touch.o
 
 
 # C Compiler Flags
@@ -95,6 +95,10 @@ ${OBJECTDIR}/_ext/48b9b4a1/hack.o: /home/dode/dev/avrtft/hack.c
 	${MKDIR} -p ${OBJECTDIR}/_ext/48b9b4a1
 	$(COMPILE.c) -g -DBAUD=9600 -DF_CPU=8000000UL -D__AVR_ATmega328P__ -D__flash=volatile -I. -std=c99 -o ${OBJECTDIR}/_ext/48b9b4a1/hack.o /home/dode/dev/avrtft/hack.c
 
+${OBJECTDIR}/_ext/48b9b4a1/i2c.o: /home/dode/dev/avrtft/i2c.c
+	${MKDIR} -p ${OBJECTDIR}/_ext/48b9b4a1
+	$(COMPILE.c) -g -DBAUD=9600 -DF_CPU=8000000UL -D__AVR_ATmega328P__ -D__flash=volatile -I. -o ${OBJECTDIR}/_ext/48b9b4a1/i2c.o /home/dode/dev/avrtft/i2c.c
+
 ${OBJECTDIR}/_ext/48b9b4a1/spi.o: /home/dode/dev/avrtft/spi.c
 	${MKDIR} -p ${OBJECTDIR}/_ext/48b9b4a1
 	$(COMPILE.c) -g -DBAUD=9600 -DF_CPU=8000000UL -D__AVR_ATmega328P__ -D__flash=volatile -I. -std=c99 -o ${OBJECTDIR}/_ext/48b9b4a1/spi.o /home/dode/dev/avrtft/spi.c
@@ -106,10 +110,6 @@ ${OBJECTDIR}/_ext/48b9b4a1/tft.o: /home/dode/dev/avrtft/tft.c
 ${OBJECTDIR}/_ext/48b9b4a1/touch.o: /home/dode/dev/avrtft/touch.c
 	${MKDIR} -p ${OBJECTDIR}/_ext/48b9b4a1
 	$(COMPILE.c) -g -DBAUD=9600 -DF_CPU=8000000UL -D__AVR_ATmega328P__ -D__flash=volatile -I. -o ${OBJECTDIR}/_ext/48b9b4a1/touch.o /home/dode/dev/avrtft/touch.c
-
-${OBJECTDIR}/i2c.o: i2c.c
-	${MKDIR} -p ${OBJECTDIR}
-	$(COMPILE.c) -g -DBAUD=9600 -DF_CPU=8000000UL -D__AVR_ATmega328P__ -D__flash=volatile -I. -o ${OBJECTDIR}/i2c.o i2c.c
 
 # Subprojects
 .build-subprojects:
